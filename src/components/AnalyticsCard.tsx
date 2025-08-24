95820fb4b678384c3213f6578f52230379fbb291
Shakiqurrahman <rahmanshakiqur@gmail.com>
2025-08-24 16:38:31 +0600

    Update AnalyticsCard styles and change body background color

--- src/components/AnalyticsCard.tsx
+++ src/components/AnalyticsCard.tsx
@@ -1,17 +1,17 @@
 type IProps = {
-  card: {
-    title: string;
-    value: string;
-  };
+    card: {
+        title: string;
+        value: string;
+    };
 };
 
 const AnalyticsCard = ({ card }: IProps) => {
-  return (
-    <div className="flex-1 bg-gray-900 p-5 rounded-lg shadow-2xl border border-gray-700">
-      <p className="text-gray-200">{card.title}</p>
-      <h2 className="text-xl font-semibold">{card.value}</h2>
-    </div>
-  );
+    return (
+        <div className="flex-1 bg-purple-600 odd:bg-teal-600 p-5 rounded-lg shadow-2xl border border-gray-700">
+            <p className="text-gray-200">{card.title}</p>
+            <h2 className="text-xl font-semibold">{card.value}</h2>
+        </div>
+    );
 };
 
 export default AnalyticsCard;
